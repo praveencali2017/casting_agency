f493beedd453287002ca216fd2c0466755effaf2
Praveen <praveenkrishnarajanagarnataraj@praveens-MBP.lan>
2020-12-17 00:25:48 -0800

    access heroku env db url

--- config.py
+++ config.py
@@ -2,6 +2,9 @@
 Later move this to env file
 """
 import logging
+import os
 logger = logging.getLogger("casting-agency-logger")
 DB_NAME = "casting_agency_db"
-DATABASE_URI = f"postgresql://postgres@localhost:5432/{DB_NAME}"
+DATABASE_URI = os.environ.get('DATABASE_URL', f"postgresql://postgres@localhost:5432/{DB_NAME}")
+
+
