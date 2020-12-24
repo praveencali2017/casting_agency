e1eee4f08af6efffc8ae1278b8b1437f83fea07b
Praveen <praveenkrishnarajanagarnataraj@praveens-MacBook-Pro.local>
2020-12-24 01:09:59 -0800

    added title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 import Dashboard from './components/Dashboard';
-
+import React, { useEffect, useState } from 'react';
 // Main App Components
+APP_TITLE = "Prav's Casting Agency";
 function App() {
+  useEffect(_=>{
+    document.title = APP_TITLE 
+  }, []);
+
   return (
     <div className="App">
       <Dashboard/>
